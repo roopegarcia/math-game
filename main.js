5453b0c62fb7d7014b9788502106728b2b941acf
rogxn <roberto.garcia@generaxion.fi>
2022-03-02 14:07:38 +0200

    Right and Wrong alerts

--- main.js
+++ main.js
@@ -1,1 +1,49 @@
-console.log('Yo! Steve here')
+// console.log('Yo! Steve here')
+
+const problemElement = document.querySelector(".problem")
+const ourForm = document.querySelector(".our-form")
+const ourField = document.querySelector(".our-field")
+
+let state = {
+  score: 0,
+  wrongAnswers: 0
+}
+
+function updateProblem() {
+  state.currentProblem = generateProblem()
+  problemElement.innerHTML = `${state.currentProblem.numberOne} ${state.currentProblem.operator} ${state.currentProblem.numberTwo}`
+}
+
+updateProblem()
+
+function generateNumber(max) {
+  return Math.floor(Math.random() * (max + 1))
+}
+
+function generateProblem() {
+  return {
+    numberOne: generateNumber(10),
+    numberTwo: generateNumber(10),
+    operator: ['+', '-', 'x'][generateNumber(2)]
+  }
+}
+
+ourForm.addEventListener("submit", handleSubmit)
+
+function handleSubmit(e) {
+  e.preventDefault()
+
+  let correctAnswer
+  const p = state.currentProblem
+  if (p.operator == "+") correctAnswer = p.numberOne + p.numberTwo
+  if (p.operator == "-") correctAnswer = p.numberOne - p.numberTwo
+  if (p.operator == "x") correctAnswer = p.numberOne * p.numberTwo
+
+  if (parseInt(ourField.value, 10) === correctAnswer) {
+    alert("Good job")
+  } else {
+    alert("Try again")
+  }
+
+
+}
